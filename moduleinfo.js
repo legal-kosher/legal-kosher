8c4cacc2395c9ceb841a60878d4d9afa73c1beb6
Dianna Faulk <diannafaulk@gmail.com>
2015-10-03 21:51:14 -0700

    Use dependency parser once dependency json created

--- moduleinfo.js
+++ moduleinfo.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var _ = require('underscore');
-
+var parseModule = require('./parse');
 // read config
 // var config = fs.readFile('./legal-config.json', 'utf-8', function(err, data) {
 //   if (err) console.log('Err readfile config.json:', err);
@@ -30,10 +30,10 @@ prc.stdout.on('end', function(info) {
     _.each(obj['dependencies'], reduceObject);
   }
   reduceObject(parsedObject);
-
-  fs.writeFile('./output.json', JSON.stringify(parsedObject), 'utf-8', function(err) {
-    if (err) console.log(err);
-  })
+  parseModule(parsedObject);
+  // fs.writeFile('./output.json', JSON.stringify(parsedObject), 'utf-8', function(err) {
+  //   if (err) console.log(err);
+  // })
 });
 
 prc.on('close', function(code) {
