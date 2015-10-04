217cecb63bfe4a6a38e995864e117621dcf81bba
Jammie Mountz <jmmountz@gmail.com>
2015-10-03 17:08:18 -0700

    tree renders through ng-repeats

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -21,30 +21,18 @@ angular.module( 'legal-kosher.about', [
 .controller( 'AboutCtrl', ['$scope', '$http', function AboutCtrl( $scope, $http ) {
   // This is simple a demo for UI Boostrap.
   
-  $scope.tree = []
+  $scope.newData;
 
   $http({
     method: 'GET',
     url: 'http://127.0.0.1:8008/mySweetData'
   }).then(function successCallback(res) {
     var dataObj = JSON.parse(res.data.data)
-    for (var key in dataObj) {
-      $scope.tree.push(dataObj[key])
-    }
+    $scope.newData = dataObj;
   }, function errorCallback(res) {
     console.log('U DUN FUGGED UP');
   });
 
-  // function parseData (data) {
-  //   console.log(data)
-  //   for (var key in data) {
-  //     if (data[key].pass) {
-
-  //     } else {
-
-  //     }
-  //   }
-  // }
   
 }])
 
